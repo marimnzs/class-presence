5ef13e41704deca0d2352f4ab255fc6300bc5636
Marina Menezes <marina@tiffins.com.br>
2024-02-25 09:52:49 -0300

    add react router

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+const App: React.FC = () => {
+
+  return (
+    <BrowserRouter>
+    <h1>hello world</h1>
+    </BrowserRouter>
+  )
+};
+
+export default App;
