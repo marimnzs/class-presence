54b8888d726609e19d5ea760df8a0a5a4c72112b
Marina Menezes <marina@tiffins.com.br>
2024-05-25 20:18:26 -0300

    add custom drawe

--- src/features/classes/index.tsx
+++ src/features/classes/index.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Drawer } from '../../components'
 
 const Classes: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
-    <h1>Classes</h1>
+  <>
+  <h1>Classes</h1>
+    <Drawer isOpen={isOpen}/>
+  </>
   )
 }
 
